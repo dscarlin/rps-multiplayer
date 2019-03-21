7baa4b9a055665566e6495db28a7ec3913784695
David Carlin <dave.carlin86@gmail.com>
2019-03-21 11:38:13 -0400

    initial files

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDPczbuDg5Sy5MCriYA5GJDVj297KqroLg",
+    authDomain: "sampleproject-f0c53.firebaseapp.com",
+    databaseURL: "https://sampleproject-f0c53.firebaseio.com",
+    projectId: "sampleproject-f0c53",
+    storageBucket: "sampleproject-f0c53.appspot.com",
+    messagingSenderId: "1002525366685"
+  };
+  firebase.initializeApp(config);
